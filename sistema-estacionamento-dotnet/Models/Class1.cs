edaa613672e71c5ce1497441202352590eecf02b
unknown <t@ACO-SIS12.acomais.intranet>
2022-09-12 16:41:24 -0300

    sistema estacionamento

---
+++ sistema-estacionamento-dotnet/Models/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistema_estacionamento_dotnet.Models
+{
+    public class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+        private string placa;
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+        }
+
+        public void AdcionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para estacionar: ");
+            placa = (Console.ReadLine());
+            veiculos.Add(placa);
+        }
+
+        public void RemoverVeiculo()
+        {
+            
+            Console.WriteLine("Digite a placa do veívulo para remover: ");
+            string placaRemover = Console.ReadLine();
+
+            //Verificar se o veículo existe
+            if (veiculos.Any(x => x.ToUpper() == placaRemover))
+                {
+                Console.WriteLine("Digite a quantidade de horas que o veículo ficou estacionado: ");
+                string qtHora = Console.ReadLine();
+                int horas = Convert.ToInt32(qtHora);
+                decimal valor = precoInicial + precoPorHora * horas;
+
+                veiculos.Remove(placaRemover);
+
+                Console.WriteLine($"O veículo {placaRemover} foi removido, preço inicial R${precoInicial}, mais o valor de horas {precoPorHora}. O preço total foi de: R${valor}");
+            } else
+            {
+                Console.WriteLine("Esssa placa não existe. Confira se digitou a placa corretamente");
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            if(veiculos.Any())
+            {
+                Console.WriteLine("Veículos estacionados: ");
+                foreach(string item in veiculos)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados!");
+            }
+        }
+    }
+}
---
+++ sistema-estacionamento-dotnet/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using sistema_estacionamento_dotnet.Models;
+
+//Permite uso de acentuação
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+decimal precoInicial = 0;
+decimal precoPorHora = 0;
+bool exibirMenu = true;
+
+
+DateTime dataAtual = DateTime.Now;
+
+try
+{
+    Console.WriteLine($"Olá, Seja bem vindo ao sistema de estacionamento! Hoje é {dataAtual} \n" + "Digite o preço inicial: ");
+    precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+    Console.WriteLine("Digite o preço por hora: ");
+    precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+} catch(Exception error_menssage)
+{
+    Console.WriteLine($"Voçe deve digitar um número!!\n{error_menssage}");
+    exibirMenu = false;
+}
+
+//Instanciando a classe estacionamento
+Estacionamento Est = new Estacionamento(precoInicial, precoPorHora);
+
+//Cadeia de caracteres vazia, somente para leitura
+string opcao = string.Empty;
+
+while(exibirMenu)
+{
+    //Console.Clear();
+    Console.WriteLine("Digite sua opção: ");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículo");
+    Console.WriteLine("4 - Sair");
+
+    opcao = Console.ReadLine();
+
+    switch(opcao)
+    {
+        case "1":
+            Est.AdcionarVeiculo();
+            break;
+
+        case "2":
+            Est.RemoverVeiculo();
+            break;
+
+        case "3":
+            Est.ListarVeiculos();
+            break;
+
+        case "4":
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+    Console.WriteLine("Pressione qualquer tecla para continuar");
+    Console.ReadLine();
+}
+
+Console.WriteLine("O programa fechou...");
